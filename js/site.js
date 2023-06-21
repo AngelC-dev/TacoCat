7cb9d40b85be3296d10fec121a92e11b2564dc33
badhddct <badhddct@yahoo.com>
2023-06-21 08:40:53 -0400

    modified:   app.html 	modified:   code.html 	new file:   img/tacocat.png 	new file:   img/tacocato.png 	modified:   index.html     modified:   js/site.js

--- js/site.js
+++ js/site.js
@@ -1,4 +1,43 @@
-//Call Hello World
-function helloWorld(){
-    alert("Hello World")
-}
+// Get the string from the page
+// Controller function
+function getValue() {
+
+    document.getElementById("alert").classList.add("invisible");
+  
+    let userString = document.getElementById("userString").value;
+    
+    let revString = reverseString(userString);
+  
+    displayString(revString);
+  }
+  
+  // Reverse the string
+  // Logic function
+  function reverseString(userString) {
+    let revString = '';
+  
+    // Remove spaces and special characters from the userString
+    userString = userString.replace(/[^a-zA-Z0-9]/g, '');
+  
+    // Reverse the string using a loop
+    for (let index = userString.length - 1; index >= 0; index--) {
+      revString += userString[index];
+    }
+  
+    // Convert the reversed string to lowercase
+    revString = revString.toLowerCase();
+  
+    return revString;
+  }
+  
+  
+  // Display the reversed string to the user
+  // View function
+  function displayString(revString) {
+    // Write to page
+    document.getElementById("msg").innerHTML = `Your phrase reversed is: ${revString}`;
+  
+    // Show the alert box
+    document.getElementById("alert").classList.remove("invisible");
+  }
+  
